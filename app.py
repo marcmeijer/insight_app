4631f49b8917ffc6266114773c0e4a91853c5907
marcamer <marcmeijer4@gmail.com>
2019-09-29 20:12:48 -0400

    interactive app

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template,request
 app = Flask(__name__)
-@app.route('/')
-def hello():
-    return "Insight web app created by marcmeijer."
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    trends = ''
+    if request.method == "POST":
+        # get url that the user has entered
+        try:
+            word = request.form['word']
+            # print statements just print to terminal
+            print("word was:")
+            print(word)
+        except:
+            print("error")
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
